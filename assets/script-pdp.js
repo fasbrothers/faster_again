fc47323b481d0cd2228759f8490d09a4b3ef7464
fasbrothers <nigmatovsanjar00@gmail.com>
2022-05-11 23:12:56 +0500

    add product description page

---
+++ assets/script-pdp.js
@@ -0,0 +1,9 @@
+var swiper2 = new Swiper(".pdpSwiper", {
+  direction: "vertical",
+  spaceBetween: 0,
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
