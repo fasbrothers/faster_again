1feec10e87a4ad0c149d010acc4025878c9e9cf2
fasbrothers <nigmatovsanjar00@gmail.com>
2022-07-27 12:13:21 +0500

    make changes in product slider

--- assets/script-pdp.js
+++ assets/script-pdp.js
@@ -1,10 +1,25 @@
 var swiper2 = new Swiper(".pdpSwiper", {
-  direction: "vertical",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
   spaceBetween: 0,
   slidesPerView: 1,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  breakpoints: {
+    0: {
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        direction: "horizontal",
+      },
+    },
+    1001: {
+      direction: "vertical",
+    },
   },
 });
 
